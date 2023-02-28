c3594e2446fcc1f9d70c02eafd430d060ab3b145
Rwyn <101754031+rwynx@users.noreply.github.com>
2023-02-28 20:18:36 +0300

    removal

--- forms/contact.php
+++ forms/contact.php
@@ -1,4 +1,5 @@
 <?php
+  /**NOT USING IT FOR NOW!!!! RRWYYN
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
@@ -7,7 +8,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'kisacamurat@gmail.com';
+  $receiving_email_address = '';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
